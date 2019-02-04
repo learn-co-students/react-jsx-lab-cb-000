e40b2581c96c051660fa1573708935da0badbb7e
Chris Cardle <cdcardle@gmail.com>
2019-02-04 20:12:45 +0000

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class fillerText extends Component {
+  render() {
+    return (
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+    )
+  }
+}
