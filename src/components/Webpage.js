554e23352c6df14b3613c6747a52c21d4eb67155
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-25 19:41:28 +0000

    Done.

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import FillerText from './FillerText'
 
 class Webpage extends React.Component {
   render() {
-    return(<div>Hello World!</div>)
+    return (
+      <div>
+        <title>The world's coolest webpage</title>
+        <FillerText />
+        <FillerText />
+      </div>
+      )
   }
 }
 
