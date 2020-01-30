3febd911c485f3c840825f0fee238e365565ad2e
Jen Poole <contact.vandepool@gmail.com>
2020-01-30 22:35:34 +0000

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export class Webpage extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <input type="password" />
+        <button type="submit" />
+      </form>
+    )
+  }
+}
