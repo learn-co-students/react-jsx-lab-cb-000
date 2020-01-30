7ea76e960b0fc13e1dbcf46eb815b2863c38dbdd
Jen Poole <contact.vandepool@gmail.com>
2020-01-30 22:38:32 +0000

    Done.

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'FillerText' from './FillerText.js';
 
 export class Webpage extends Component {
   render() {
     return (
-      <form>
-        <input type="text" />
-        <input type="password" />
-        <button type="submit" />
-      </form>
+      <div>
+        <h1>The world&#39;s coolest webpage</>
+        <FillerText />
+        <FillerText />
+      </div>
     )
   }
 }
